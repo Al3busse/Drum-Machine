d4a87dd315ede59b5c69634f6c0db8a60477a49a
Alejandro Busse <al3busse@gmail.com>
2020-05-13 07:47:20 -0300

    Updates

---
+++ precache-manifest.cc12790e675d9ee77cd7cd11c6ccefa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895abb024b72b7364dfb44edb54b7655",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "cfb965f6a74cc2447cff",
+    "url": "/Drum-Machine/static/css/main.043e2873.chunk.css"
+  },
+  {
+    "revision": "055e73cf9130316af902",
+    "url": "/Drum-Machine/static/js/2.64fd74c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Drum-Machine/static/js/2.64fd74c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb965f6a74cc2447cff",
+    "url": "/Drum-Machine/static/js/main.67336c05.chunk.js"
+  },
+  {
+    "revision": "49000b987a454d2a4a0d",
+    "url": "/Drum-Machine/static/js/runtime-main.ffe54c2e.js"
+  },
+  {
+    "revision": "ce4b0ee545ec52c2ec8cec62d23ffb09",
+    "url": "/Drum-Machine/static/media/Calculator.ce4b0ee5.ttf"
+  }
+]);
